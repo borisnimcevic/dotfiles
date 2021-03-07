83f1eba04ac8786b1d27ef01eb444703504049f6
Boris Nimcevic <borisnimcevic@gmail.com>
2021-03-07 12:04:59 +0100

    Vimspector done.

--- scripts/nvim-setup.sh
+++ scripts/nvim-setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# get curl
+# prerequisits
 sudo apt update
+sudo apt install -y clangd
 sudo apt install -y curl
 sudo apt install -y git
+sudo apt install -y nodejs
+sudo apt install -y npm
+sudo apt install -y python3.9
+sudo apt install -y python3-pip
+pip3 install neovim
 
 # get neovim
 curl -LOf https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
@@ -21,3 +27,12 @@ ln -s $HOME/dotfiles/.config/nvim $HOME/.config
 # get vim plugged
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# install all the Plugs
+nvim --headless +PlugInstall +qa
+
+# setup coc
+nvim --headless +"CocInstall coc-clangd" +qa
+
+# setup vimspector
+nvim --headless +"VimspectorInstall vscode-cpptools" +qa
