98f44892e4f54a515bc0ed785ee82ce5b3818e93
Boris Nimcevic <borisnimcevic@gmail.com>
2021-03-07 13:48:42 +0100

    First shot at nvim setup script.

--- scripts/nvim-setup.sh
+++ scripts/nvim-setup.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-# prerequisits
+# install prerequisites
 sudo apt update
-sudo apt install -y clangd
-sudo apt install -y curl
-sudo apt install -y git
-sudo apt install -y nodejs
-sudo apt install -y npm
-sudo apt install -y python3.9
-sudo apt install -y python3-pip
+
+function install-prog {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo apt install -y $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+install-prog clangd
+install-prog curl
+install-prog figlet
+install-prog git
+install-prog nodejs
+install-prog npm
+install-prog python3.9
+install-prog python3-pip
+
 pip3 install neovim
+
+# add path
+export PATH="$HOME/.local/bin/:$PATH"
+
+# install language servers
+npm i -g bash-language-server
+pip3 install cmake-language-server
 
 # get neovim
 curl -LOf https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
@@ -18,8 +39,11 @@ curl -LOf https://github.com/neovim/neovim/releases/download/nightly/nvim.appima
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 
-# add it as a maing editor
+# add it as a main editor
 echo "export EDITOR=\"nvim\"" >> $HOME/.bash_profile
+
+# make alias
+echo "alias v=\"nvim\"" >> $HOME/.bashrc
 
 # symlink nvim configuration
 ln -s $HOME/dotfiles/.config/nvim $HOME/.config
@@ -33,6 +57,13 @@ nvim --headless +PlugInstall +qa
 
 # setup coc
 nvim --headless +"CocInstall coc-clangd" +qa
+nvim --headless +"CocInstall coc-cmake" +qa
+nvim --headless +"CocInstall coc-pyright" +qa
+nvim --headless +"CocInstall coc-sh" +qa
+nvim --headless +"CocInstall coc-spell-checker" +qa
 
 # setup vimspector
 nvim --headless +"VimspectorInstall vscode-cpptools" +qa
+
+# reference to @ThePrimeagen
+figlet "coconut  oil  applied" -f small
