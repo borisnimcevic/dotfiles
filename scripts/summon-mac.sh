19e3d3c0dc0a48d5b363281f20428afc20d59204
Boris Nimcevic <borisnimcevic@gmail.com>
2022-10-08 15:54:44 +0200

    Setup tmux

--- scripts/summon-mac.sh
+++ scripts/summon-mac.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Brew setup
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/borisnimcevic/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/borisnimcevic/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Tmux setup
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm    
+brew install tmux
+# The first time you run tmux install the plugins by running "prefix + I" (that a capital i)
