ed7b0931404d3e2af2088b1eadb36f71c397c2e2
Boris Nimcevic <borisnimcevic@gmail.com>
2023-04-26 19:57:06 +0200

    Moved variables to a separate file

---
+++ .zsh/variables.zsh
@@ -0,0 +1,8 @@
+export EDITOR='nvim' # neovim is default editor
+
+export IDF_PATH="$HOME/esp/idf_5.0/esp-idf" # set it as a default
+
+export CPPUTEST_HOME=/home/borisnotes/tools/cpputest/cpputest-3.8
+
+export NVM_DIR="$HOME/.nvm"
+
