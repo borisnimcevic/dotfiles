b184dc2b08f979fff166d43f73225e2372068149
Boris Nimcevic <borisnimcevic@gmail.com>
2023-04-26 20:03:02 +0200

    Moved commented config out

---
+++ .zsh/config_extras.zsh
@@ -0,0 +1,37 @@
+# ----------------
+# Alternate Themes
+# ----------------
+# ZSH_THEME="fletcherm"
+# ZSH_THEME="dracula"
+# ZSH_THEME="boris"
+# ZSH_THEME="amuse"
+# ZSH_THEME="agnoster"
+
+# ----------------
+# Dracula Theme Parameters
+# ----------------
+# DRACULA_DISPLAY_TIME=1
+# DRACULA_DISPLAY_CONTEXT=0
+
+# ----------------
+# Terminal Color Configuration
+# ----------------
+# export TERM=xterm-256color
+
+# ----------------
+# Custom Prompt (Commented Out)
+# ----------------
+# update_prompt() {
+#   NUM=$(cat /Users/borisnimcevic/quandify/labday/2023/chatgpt-metro/a.txt)
+#   echo "%n - %{$fg[green]%}Leave in $NUM min.%{$reset_color%}%\> "
+# }
+# NUM=$(cat /Users/borisnimcevic/quandify/labday/2023/chatgpt-metro/a.txt)
+# PS1='$(update_prompt)'
+
+# ----------------
+# Tmux Autostart (Commented Out)
+# ----------------
+# if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
+#     tmux attach -t default || tmux new -s default
+# fi
+
