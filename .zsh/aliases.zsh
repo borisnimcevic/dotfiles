3cee8742530a5dbc58161354db728a7fb591ea0b
Boris Nimcevic <boris.nimcevic@quandify.com>
2025-03-21 11:54:10 +0100

    fix(quandify): general work updates

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,29 @@
+# Helper functions
+
+disable_chpwd_run_and_restore() {
+    # Save the current chpwd function if it exists
+    if declare -F chpwd > /dev/null; then
+        original_chpwd=$(declare -f chpwd)
+        unset -f chpwd  # Temporarily disable the chpwd function
+    fi
+
+    # Execute the command block passed to this function
+    "$@"
+
+    # Restore the original chpwd function, if it was set
+    if [[ -n "$original_chpwd" ]]; then
+        eval "$original_chpwd"
+    fi
+}
+
+# get_idf3() {
+#     disable_chpwd_run_and_restore {
+#         export IDF_PATH=$HOME/esp/idf_5.3/esp-idf
+#         . "$HOME/esp/idf_5.3/esp-idf/export.sh"
+#         source "$HOME/esp/idf_5.3/esp-matter/export.sh"
+#     }
+# }
+
 # General:
 alias :q="exit" # vim habit
 alias :w="figlet \"lol, wtf?\" | lolcat" # vim habit
@@ -11,13 +37,14 @@ alias mv="mv -v"
 alias o="open"
 alias rm="rm -rv"
 alias v="nvim"
-
+alias cdate="date +%Y-%m-%d | pbcopy"
 # Navigation:
 alias aliases="cd $HOME/dotfiles/.zsh && nvim aliases.zsh && cd -"
 alias desk="cd $HOME/Desktop/."
 alias down="cd $HOME/Downloads/."
 alias play="cd $HOME/my_corner/projects/playground/."
 alias pro="cd $HOME/my_corner/projects/."
+alias junk="cd $HOME/junk/."
 
 # TMUX:
 alias priv='tmux new -s priv'
@@ -39,28 +66,37 @@ alias msp_clean="/Applications/ti/ccs1210/ccs/utils/bin/gmake -j 7 clean -O"
 
 # Espressif:
 alias get_idf4='export IDF_PATH=$HOME/esp/idf_4.4/esp-idf && . $HOME/esp/idf_4.4/esp-idf/export.sh' # CS,CDet
-alias get_idf5='export IDF_PATH=$HOME/esp/idf_5.0/esp-idf && . $HOME/esp/idf_5.0/esp-idf/export.sh' # CT
-alias get_idf2='export IDF_PATH=$HOME/esp/idf_5.2/esp-idf && . $HOME/esp/idf_5.2/esp-idf/export.sh' # CDot
+alias get_idf3='export IDF_PATH=$HOME/esp/idf_5.3/esp-idf && . $HOME/esp/idf_5.3/esp-idf/export.sh'
+# alias get_idf3='export IDF_PATH=$HOME/esp/idf_5.3/esp-idf && . $HOME/esp/idf_5.3/esp-idf/export.sh && source $HOME/esp/idf_5.3/esp-matter/export.sh' # CDot
+# alias get_idf3='disable_chpwd_run_and_restore bash -c "{ export IDF_PATH=$HOME/esp/idf_5.3/esp-idf && . $HOME/esp/idf_5.3/esp-idf/export.sh && source $HOME/esp/idf_5.3/esp-matter/export.sh; }"'
+# alias get_idf3='disable_chpwd_run_and_restore sh -c "{ export IDF_PATH=$HOME/esp/idf_5.3/esp-idf && . $HOME/esp/idf_5.3/esp-idf/export.sh && source $HOME/esp/idf_5.3/esp-matter/export.sh; }"'
 alias idfb='idf.py build'
 alias idff='idf.py flash'
 alias idfm='idf.py monitor'
+alias idffm='idf.py flash monitor'
 
 # Quandify:
 alias build="./build.sh"
 alias clean="./clean.sh"
 alias lecture="cd /Users/borisnimcevic/quandify/knowledge/unit_testing/course && docker run -it --rm -v $PWD:/lab throwtheswitch/drsurly-course1"
+alias qcd="cd $HOME/code/cubic-dot-fw/."
+alias qcda="cd $HOME/code/cubic-dot-fw/esp32-app/."
+alias qcdet="cd $HOME/code/firmware/products/cubic-detector/."
 alias qcdetesp="cd $HOME/code/firmware/products/cubic-detector/esp32/."
-alias qhil="$HOME/code/firmware/products/cubic-detector/hil/esp-controller/esp32s3_hil_controller/"
+alias qcdf="cd $HOME/code/cubic-dot-fw/esp32-factory/."
 alias qcdm="cd $HOME/code/firmware/products/cubic-dot/msp-cubic-dot"
-alias qcdot="cd $HOME/code/cubic-dot-fw/."
+alias qcdt="cd $HOME/code/cubic-dot-fw/tools/."
 alias qcm="cd $HOME/code/firmware/products/cubic-meter"
 alias qcs="cd $HOME/code/firmware/products/cubic-secure"
 alias qcse="cd $HOME/code/firmware/products/cubic-secure/esp32/."
+alias qcst="cd $HOME/code/firmware/products/cubic-secure/esp32/test"
 alias qct="cd $HOME/code/cubic-tool/cubic-tool-fw/."
 alias qcte="cd $HOME/code/cubic-tool/cubic-tool-fw/esp32/."
+alias qcts="cd $HOME/code/cubic-tool/cubic-tool-fw/stm32/Core/Src/."
 alias qe="cd $HOME/code/environments/images"
 alias qfir="cd $HOME/code/firmware/."
 alias qft="cd $HOME/code/firmware-tools"
+alias qhil="$HOME/code/firmware/products/cubic-detector/hil/esp-controller/esp32s3_hil_controller/"
 alias qhp="cd $HOME/code/hardware-python"
 alias qmux='tmux new -s quan'
 alias qssh="ssh hil@192.168.1.91"
--- .zshrc
+++ .zshrc
@@ -13,6 +13,11 @@ source $HOME/.zsh/functions.zsh
 source $HOME/.zsh/variables.zsh
 
 # ----------------
+# Language
+# ----------------
+export LANG=en_US.UTF-8
+
+# ----------------
 # Oh-My-Zsh
 # ----------------
 export ZSH="$HOME/.oh-my-zsh"
@@ -74,3 +79,10 @@ export PATH="/Users/borisnimcevic/sonar/bin:$PATH"
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 export PATH="/Users/borisnimcevic/my_corner/llvm-project/clang-tools-extra/clang-tidy/tool:$PATH"
 
+
+export STM32_PRG_PATH=/Applications/STMicroelectronics/STM32Cube/STM32CubeProgrammer/STM32CubeProgrammer.app/Contents/MacOs/bin
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/borisnimcevic/junk/pipelines/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/borisnimcevic/junk/pipelines/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/borisnimcevic/junk/pipelines/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/borisnimcevic/junk/pipelines/google-cloud-sdk/completion.zsh.inc'; fi
